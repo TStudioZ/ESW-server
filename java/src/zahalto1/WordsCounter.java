3ef3b1f2eb5ac51faaf67e82fa2948b01e55b574
Tomáš Zahálka <zahalto1@fel.cvut.cz>
2018-05-19 17:51:45 +0200

    New Java submit, measured 2.043 s. Using ConcurrentHashMap.newKeySet().

--- java/src/zahalto1/WordsCounter.java
+++ java/src/zahalto1/WordsCounter.java
@@ -1,24 +1,28 @@
 package zahalto1;
 
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 public class WordsCounter {
 
-    private final ConcurrentMap<String, Integer> words;
+    //private final ConcurrentMap<String, Integer> words;
+    private final ConcurrentHashMap.KeySetView<String, Boolean> wordsSet;
 
     public WordsCounter() {
-        words = new ConcurrentHashMap<>();
+        //this.words = new ConcurrentHashMap<>();
+        this.wordsSet = ConcurrentHashMap.newKeySet(300000);
     }
 
     public void addWord(String word) {
         //words.merge(word, 1, (oldCount, count) -> oldCount + count);
-        words.put(word, 1);
+        //words.put(word, 1);
+        wordsSet.add(word);
     }
 
     public synchronized int getWordCountReset() {
-        int count = words.size();
-        words.clear();
+        //int count = words.size();
+        //words.clear();
+        int count = wordsSet.size();
+        wordsSet.clear();
         return count;
     }
 }

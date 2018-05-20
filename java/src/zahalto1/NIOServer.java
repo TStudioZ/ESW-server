e453b471d1870b666f11fe3b15b439f5ea55c02f
Tomáš Zahálka <zahalto1@fel.cvut.cz>
2018-05-20 13:41:02 +0200

    New Java submit, measured 1.327 s.

--- java/src/zahalto1/NIOServer.java
+++ java/src/zahalto1/NIOServer.java
@@ -12,7 +12,7 @@ public class NIOServer {
 
     static {
         bufferPool = new BufferPool();
-        reactors = new NIOReactor[4];
+        reactors = new NIOReactor[16];
         try {
             for (int i = 0; i < reactors.length; i++) {
                 reactors[i] = new NIOReactor();
